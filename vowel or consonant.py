126bd83526a1715bca50c14267b634502bc5915d
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-09-01 21:12:05 +0530

    Create vowel or consonant.py

---
+++ vowel or consonant.py
@@ -0,0 +1,8 @@
+letter=str(raw_input())
+if (letter.isalpha()):
+	if(letter=='a' or letter=='e' or letter=='i' or letter=='o' or letter=='u' or letter=='A' or letter=='E' or letter=='I' or letter=='O' or letter== 'U'):
+       		print("vowel")
+	else:
+    		print("consonant")
+else:
+            print("invalid")
