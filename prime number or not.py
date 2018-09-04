4d2ff055b4d059f0d82a480e077d50536455effa
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-09-04 12:31:45 +0530

    Update prime number or not.py

--- prime number or not.py
+++ prime number or not.py
@@ -1,7 +1,7 @@
-number = int(input( ))
-if number > 1:
-    for i in range(2, number):
-        if (number % i) == 0:
+n = int(input( ))
+if n > 1:
+    for i in range(2, n):
+        if (n % i) == 0:
          print("no")
             break
     else:
