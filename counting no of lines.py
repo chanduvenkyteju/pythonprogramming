89d2c7d35c31e12633192b2b78e5b511b424b1ad
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-11-23 20:22:00 +0530

    Update counting no of lines.py

--- counting no of lines.py
+++ counting no of lines.py
@@ -1,8 +1,8 @@
-n=input()
-count=1
-for x in range(0,len(n)):
-    if(n[x]=="."):
-        count=count+1
+x=input()
+c=1
+for i in range(0,len(x)):
+    if(x[i]=="."):
+        c=c+1
     else:
-        count=count
-print(count)
+        c=c
+print(c)
