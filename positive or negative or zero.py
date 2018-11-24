38ccf86c60b25b14cb8ec566f9440d67cae377dc
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-11-24 11:47:08 +0530

    Update positive or negative or zero.py

--- positive or negative or zero.py
+++ positive or negative or zero.py
@@ -1,8 +1,7 @@
-P=str(input())
-if P.isdigit():
-      if P<0:
-	 print("Negative")
-      elif P==0:
-	 print("Zero")
-      else:
-	 print("Positive")
+P=input()
+if P<0:
+    print("Negative")
+elif P==0:
+    print("Zero")
+else:
+    print("Positive")
