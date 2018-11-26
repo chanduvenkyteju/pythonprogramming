6fac0b33837b770f7e33e983ba854c6b329ef8ba
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-11-26 09:16:04 +0530

    Update converting a sentence in to a camelcase.py

--- converting a sentence in to a camelcase.py
+++ converting a sentence in to a camelcase.py
@@ -1,6 +1,6 @@
 def cond(word):
         import re
-        return ''.join(x.capitalize() or ' ' for x in word.split(' ')) 
+        return ''.join(x.capitalize() or " " for x in word.split(" ")) 
     
 n=input()
 print(cond(n))
