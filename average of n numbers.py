3cedcc7688e799a5026fcff624aa0bada52dd9d3
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-11-27 21:16:35 +0530

    Update and rename average of n numbers.py to avg of n num.py

--- average of n numbers.py
+++
@@ -1,6 +0,0 @@
-n=int(input())
-a=[]
-for i in range(0,n):
-    a.append(int(input()))
-t=sum(a)/n    
-print(t)
---
+++ avg of n num.py
@@ -0,0 +1,6 @@
+p=int(raw_input())
+q=[]
+for i in range(0,p):
+    q.append(int(input()))
+m=sum(q)/p    
+print(m)
