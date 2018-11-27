97481610c161dac4256e0205d66cec559217ba17
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-11-27 18:19:32 +0530

    Update and rename counting number of prime numbers in a given range.py to count primes .py

--- count primes .py
+++ count primes .py
@@ -1,11 +1,11 @@
-p = int(input( ))
-q = int(input( ))
-count=0
+p = int(raw_input( ))
+q = int(raw_input( ))
+c=0
 for n in range(p,q ):
    if n > 1:
        for i in range(2,n):
            if (n % i) == 0:
                break
        else:
-           count=count+1
-print(count)  
+           c=c+1
+print(c)  
