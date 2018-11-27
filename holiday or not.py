1161059667da424a471ae173f5e4916a563465ab
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-11-27 11:59:15 +0530

    Create holiday or not.py

---
+++ holiday or not.py
@@ -0,0 +1,6 @@
+v=['Monday','Tuesday','Wednesday','Thursday','Friday','monday','tueaday','wednesday','thursday','friday','saturday']
+d=str(input())
+if (d in v):
+    print("no")
+else:
+    print("yes")
