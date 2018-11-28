8af508584781efa6a48b357ab8dffb8ec6c69784
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-11-28 07:40:41 +0530

    Update primes of num.py

--- primes of num.py
+++ primes of num.py
@@ -1,13 +1,13 @@
-n=int(input())
+N=int(input())
 i=1
-while(i<=n):
-    k=0
-    if(n%i==0):
+while(i<=N):
+    K=0
+    if(N%i==0):
         j=1
         while(j<=i):
             if(i%j==0):
-                k=k+1
+                K=K+1
             j=j+1
-        if(k==2):
+        if(K==2):
             print(i,end=' ')
     i=i+1
