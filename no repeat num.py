b8e1acf4094e1aa3b870bafe49524d3a9e00da86
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-11-28 11:52:36 +0530

    Create no repeat num.py

---
+++ no repeat num.py
@@ -0,0 +1,9 @@
+N=int(raw_input())
+a=[]
+for i in range(0,N):
+    a.append(int(input()))
+for j in range(0,len(a)):
+    for k in range(0,len(a)):
+        if(a[j]!=a[k]):
+            break
+        print(a[j])
