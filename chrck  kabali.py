7d08adeff940a5ab225e768ab9551985cbb0cd24
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-11-28 11:00:59 +0530

    Update and rename anagram of kabali.py to chrck  kabali.py

--- chrck  kabali.py
+++ chrck  kabali.py
@@ -1,9 +1,9 @@
-N=int(input())
+N=int(raw_input())
 a=[]
 for i in range(0,N):
     a.append(str(input()))
 c=0
-S="kabali"
+S=str(raw_input)
 for i in range(0,N):
     if(sorted(a[i])==sorted(S)):
         c=c+1
