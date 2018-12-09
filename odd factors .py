dd2b4de2e52674b7747e1eeaffb695ab6b7e0eac
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-12-09 15:17:18 +0530

    Update odd factors .py

--- odd factors .py
+++ odd factors .py
@@ -1,7 +1,7 @@
 N=int(raw_input())
-a=[]
+M=[]
 for i in range(1,N+1):
     if(N%i==0 and i%2!=0):
-        a.append(i)
-s=" ".join(map(str,a))
+        M.append(i)
+s=" ".join(map(str,M))
 print(s)
