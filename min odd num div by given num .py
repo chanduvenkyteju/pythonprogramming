78978e5b1b1c7ee400845e31cce42daeb2487958
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-12-15 11:34:22 +0530

    Update min odd num div by given num .py

--- min odd num div by given num .py
+++ min odd num div by given num .py
@@ -1,8 +1,8 @@
-N=int(raw_input())
+N =int(raw_input())
 if(N%2!=0):
     print("1")
 else:
     for i in range(2,N):
-        if(N%i==0 and (int)((N/i)%2==1)):
+        if(N%i==0 and (int)((N/i) %2==1)):
             print(i)
-            break;
+            break
