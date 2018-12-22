0abd73c2b1ecf986694231d6d2e38ce132a424bc
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-12-22 19:56:13 +0530

    Update same num with space.py

--- same num with space.py
+++ same num with space.py
@@ -2,11 +2,11 @@ z=1
 y=1
 x=[]
 a=int(raw_input())
-v=len(str(a))
+p=len(str(a))
 y=a
 while(a>0):
     z=a%10
     a=a//10
     x.append(z)
-for i in range(v-1,-1,-1):
+for i in range(p-1,-1,-1):
     print(x[i],end=" ")
