beefd32d39d75c4d7651c5572ac079d24fee3d31
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-12-25 12:12:36 +0530

    Update conv X to Y.py

--- conv X to Y.py
+++ conv X to Y.py
@@ -2,8 +2,8 @@ X,Y=(raw_input()).split()
 X=list(X)
 Y=list(Y)
 p=abs(len(X)-len(Y))
-n=len(min(X,Y))
-for i in range(n):
+n=len(max(X,Y))
+for i in range(n-1):
     if X[i]!=Y[i]:
         p=p+1
 print(p)
