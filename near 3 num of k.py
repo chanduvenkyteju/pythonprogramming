85cfe45fc6f407d21cf9a67a7f9dc91d9092564c
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-12-25 13:36:14 +0530

    Update near 3 num of k.py

--- near 3 num of k.py
+++ near 3 num of k.py
@@ -5,13 +5,13 @@ K=list(map(int,K))
 if N[1] not in K:
   K.append(N[1])
 K=sorted(K)
-m=K.index(N[1])
-n=len(K)
-if(m==0):
+p=K.index(N[1])
+q=len(K)
+if(p==0):
   print(K[1],K[2],K[3],sep=' ')
-elif(m==1):
+elif(p==1):
   print(K[0],K[2],K[3],sep=' ')
-elif(m==n-1):
-  print(K[n-2],K[n-3],K[n-4],sep=' ')
+elif(p==q-1):
+  print(K[q-2],K[q-3],K[q-4],sep=' ')
 else:
-  print(K[m-1],K[m+1],K[m-2],sep=' ')
+  print(K[p-1],K[p+1],K[p-2],sep=' ')
