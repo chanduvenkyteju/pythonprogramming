117ecc5e8220dc5497db7d86bc5d7e430aca6acf
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2018-12-26 22:06:19 +0530

    Create K no of exist num.py

---
+++ K no of exist num.py
@@ -0,0 +1,12 @@
+N,K=(raw_input().split())
+N=int(N)
+K=int(K)
+c=0
+b=[]
+a=(raw_input()).split()
+for i in a:
+    if(a.count(i)==K):
+        b.append(i)
+        while i in a:
+            a.remove(i)
+print(*b)
