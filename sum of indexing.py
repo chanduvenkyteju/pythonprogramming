97c609cef9257a2609beccc1ebcf17252668664c
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2019-01-06 13:22:24 +0530

    Create sum of indexing.py

---
+++ sum of indexing.py
@@ -0,0 +1,8 @@
+N,Q=map(int,(raw_input()).split())
+l=list(map(int,(raw_input()).split()))
+for i in range(0,Q):
+    U,V=map(int,(raw_input()).split())
+    s=0
+    for j in range(U-1,V):
+        s=s+l[j]
+    print(s)
