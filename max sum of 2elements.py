c52f4e93ede2627fd796857a6b79e83fc7b087a1
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2019-01-06 17:34:48 +0530

    Update max sum of 2elements.py

--- max sum of 2elements.py
+++ max sum of 2elements.py
@@ -1,8 +1,9 @@
 N=int(raw_input())
-a=(raw_input()).split()
+a=list(map(int,(raw_input()).split()))
 s=0
 c=[]
-for x in a:
-    s=s+int(x)
-    c.append(s)
+for i in a:
+    for j in range(i+1,N):
+        s=a[i]+a[j]
+        c.append(s)
 print(max(c))
