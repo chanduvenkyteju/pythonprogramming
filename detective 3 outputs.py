8988a4baf310cc7a71d0f90560cd0f39532b3ba2
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2019-01-06 12:03:44 +0530

    Create detective 3 outputs.py

---
+++ detective 3 outputs.py
@@ -0,0 +1,10 @@
+N=int(raw_input())
+a=list(map(int,(raw_input()).split()))
+b=[]
+for i in range(1,N):
+    c=0
+    for j in range(i):
+        if(a[i]>a[j]):
+            c=c+a[j]
+    b.append(c)
+print(sum(b))
