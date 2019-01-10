20c319e2d04bcab381a456a7bce0467348c6894d
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2019-01-10 20:19:22 +0530

    Update rem a substr.py

--- rem a substr.py
+++ rem a substr.py
@@ -1,8 +1,11 @@
 N=(raw_input())
 M=(raw_input())
 a=[]
+l=int(len(a))
 if(M in N):
     a=N.replace(M,"")
-for i in a:
-    a=a.replace("  "," ")
-print("".join(str(i) for i in a))  
+if(a[0]==" "):
+    a=a.replace(" ","")    
+for i in a:   
+    a=a.replace("  "," ")        
+print("".join(str(i) for i in a))
