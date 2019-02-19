35c27cf694586a9a77a1697c38766737ad1e8832
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2019-02-19 23:10:41 +0530

    Update meeting room in flipkart.py

--- meeting room in flipkart.py
+++ meeting room in flipkart.py
@@ -1,13 +1,15 @@
 n=int(raw_input())
-a=list(map(int,(raw_input()).split()))
-b=list(map(int,(raw_input()).split()))
+a=list(map(eval,(raw_input()).split()))
+b=list(map(eval,(raw_input()).split()))
 c=1
-for i in range(n-1):
-    for j in range(i+1,n):
-        if(a[j]>=b[i]):
-            i=j
-            c=c+1
-            break
-    if(i==n-1):
-        break
-print(c)
+i=0
+while (i<n-1):
+  j=i+1
+  while(j<n):
+    if(b[i]<=a[j]):
+      c+=1
+      i=j-1
+      break
+    j+=1
+  i+=1
+print(str(c))
