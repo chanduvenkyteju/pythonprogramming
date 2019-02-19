b531dc119dcad1e71713ad0915e4bee012898bb0
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2019-02-19 23:46:55 +0530

    Update min element in every insert.py

--- min element in every insert.py
+++ min element in every insert.py
@@ -1,9 +1,11 @@
 n=int(raw_input())
-a=list(map(int,(raw_input()).split()))
-b=[]
-for i in range(0,n):
-    b.append(a[i])
-    if(len(b)<len(a)):
-        print(min(b),end=" ")
+a=input().strip().split(" ")
+ans=""
+mi=1000000
+for x in a:
+    if int(x)<mi:
+        mi=int(x)
+        ans+=x+" "
     else:
-        print(min(b))
+        ans+=str(mi)+" "
+print(ans.strip())
