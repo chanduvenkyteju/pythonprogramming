8cb91dd01f66d0409f481d27ce2ae4d389b64d21
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2019-02-20 23:23:42 +0530

    Create weight&value arrays.py

---
+++ weight&value arrays.py
@@ -0,0 +1,17 @@
+n,w=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+d=[]
+c=0
+for i in range(n):
+    x=b[i]/a[i]
+    d.append(x)
+while w>=0 and len(d)>0:
+    mindex=d.index(max(d))
+    if w>=a[mindex]:
+        c=c+b[mindex]
+        w=w-a[mindex]
+    a.pop(mindex)
+    b.pop(mindex)
+    d.pop(mindex)
+print(c)
