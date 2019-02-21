ceb03d9a45c8c132673f75e2c6641a54d1594904
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2019-02-21 13:20:16 +0530

    Create ab subsequence.py

---
+++ ab subsequence.py
@@ -0,0 +1,20 @@
+def fun(s,z):
+    l=z+1
+    if l==len(s):
+        return 1
+    else:
+        if s[z]=='a' and s[l]=='b':
+            return 1+fun(s,l)
+        if s[z]=='b' and s[l]=='a':
+            return 1+fun(s,l)
+        else:
+            return 1
+n=(raw_input())
+t=[]
+for z in range(len(n)-1):
+    if n[z]=='a' and n[z+1]=='b':
+        t.append(fun(n,z))
+if len(t)==0:
+    print(0)
+else:
+    print(maz(t))
