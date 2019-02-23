284c0aa9e51502f58e9f6ad506613a256a481279
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2019-02-23 11:54:54 +0530

    Update sum of 2 num =0.py

--- sum of 2 num =0.py
+++ sum of 2 num =0.py
@@ -1,7 +1,9 @@
 N=int(raw_input())
 a=list(map(int,(raw_input()).split()))
+k=max(a)
 for i in range(0,N-1):
     for j in range(i+1,N):
-        if(a[i]+a[j]==0):
+        if((a[i]+a[j]==0) or (a[i]+a[j]<k)):
             print(a[i],a[j])
             break
+    break
