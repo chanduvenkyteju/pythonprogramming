d5cb03d257bc544ab0fc60166b01d6eb3d58f171
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2019-03-26 18:42:50 +0530

    Create lexographical ascending order.py

---
+++ lexographical ascending order.py
@@ -0,0 +1,15 @@
+n=int(raw_input())
+a=(raw_input().split())
+a.sort()
+l=[]
+s=[]
+for i in a:
+    l.append(len(i))
+l.sort()
+for x in l:
+    for y in a:
+        if(len(y)==x):
+            if y not in s:
+                s.append(y)
+for i in s:
+    print(i,end=" ")
