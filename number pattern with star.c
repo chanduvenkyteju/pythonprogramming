a7160f2c9d7a3dd7c08e38b0af58c3cd70332913
chanduvenkyteju <42645538+chanduvenkyteju@users.noreply.github.com>
2019-07-26 15:09:36 +0530

    Update number pattern with star.c

--- number pattern with star.c
+++ number pattern with star.c
@@ -16,3 +16,22 @@ int main()
     }
     return 0;
 }
+//for higher numbers
+#include <stdio.h>
+int main()
+{
+    int n=4,m=4, i, j, k;
+    k = 13;
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=m; j++, k++)
+        {
+            if(j!=1)
+                printf("*%d", k);
+            else
+                printf("%d", k);
+        }
+        printf("\n");
+    }
+    return 0;
+}
